56dd7b9538b6fd1bd03b0aefc9ca448658bf85b1
Caijinglong <cjl_spy@163.com>
2019-04-01 09:36:36 +0800

    add a compile common shell

---
+++ ios/compile-common.sh
@@ -0,0 +1,8 @@
+./compile-openssl.sh arm64 
+./compile-openssl.sh x86_64 
+./compile-openssl.sh i386
+./compile-openssl.sh lipo
+./compile-ffmpeg.sh arm64 
+./compile-ffmpeg.sh x86_64 
+./compile-ffmpeg.sh i386
+./compile-ffmpeg.sh lipo
