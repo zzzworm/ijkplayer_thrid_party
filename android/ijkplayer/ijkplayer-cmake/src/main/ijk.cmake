9fcfe59dcfac9ad8fe5a0657645e97135d1ade37
cjl_macbook <cjl_spy@163.com>
2020-03-13 21:13:22 +0800

    初步完成了Cmake支持

--- android/ijkplayer/ijkplayer-cmake/src/main/ijk.cmake
+++ android/ijkplayer/ijkplayer-cmake/src/main/ijk.cmake
@@ -1,5 +1,13 @@
+message("arch abi = ${CMAKE_ANDROID_ARCH_ABI}")
+
 message("include the ijk.cmake start")
 file(GLOB IJK_SRC cpp/*.c cpp/*.cpp)
 message("c files: ${IJK_SRC}")
-add_library(ijkplayer SHARED ${IJK_SRC})
+#add_library(ijkplayer SHARED ${IJK_SRC})
+add_library(ijkffmpeg SHARED IMPORTED)
+set_target_properties(ijkffmpeg PROPERTIES IMPORTED_LOCATION ${FFMPEG_PATH})
+
+message("ijk media path = ${IJK_PATH}/ijkmedia")
+add_subdirectory(${IJK_PATH}/ijkmedia ijkmedia)
+
 message("include the ijk.cmake end")
